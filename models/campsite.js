12a0299fb25d14a63b12406a1baa577909709a24
chrisbrooksla@gmail.com <chrisbrooksla@gmail.com>
2021-03-12 12:01:43 -0600

    Week_2_Workshop

--- models/campsite.js
+++ models/campsite.js
@@ -4,6 +4,25 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 const campsiteSchema = new Schema({
     name: {
         type: String,
@@ -36,20 +55,20 @@ const campsiteSchema = new Schema({
     timestamps: true
 });
 
-const campsiteSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    comments: [commentSchema]
-}, {
-    timestamps: true
-});
+// const campsiteSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     comments: [commentSchema]
+// }, {
+//     timestamps: true
+// });
 
 const Campsite = mongoose.model('Campsite', campsiteSchema);
 
